c62f8f0706a0e0ae5c35799650944a7090040c76
Jacob Hotz <77470805+Jacob1010-h@users.noreply.github.com>
2023-12-07 18:10:10 -0800

    add boot with env

---
+++ boot.py
@@ -0,0 +1,36 @@
+# This file is executed on every boot (including wake-boot from deepsleep)
+# import esp
+# esp.osdebug(None)
+def do_connect(ssid, pwd):
+    import network
+
+    sta_if = network.WLAN(network.STA_IF)
+    if not sta_if.isconnected():
+        print("connecting to network...")
+        sta_if.active(True)
+        sta_if.connect(ssid, pwd)
+        while not sta_if.isconnected():
+            pass
+    print("network config:", sta_if.ifconfig())
+
+
+# This file is executed on every boot (including wake-boot from deepsleep)
+# import esp
+# esp.osdebug(None)
+
+# Attempt to connect to WiFi network
+import os
+from dotenv import load_dotenv
+
+load_dotenv("./net.env")
+
+do_connect(
+    os.getenv("NAME"),
+    os.getenv("PASSWORD")
+)
+
+import webrepl
+
+webrepl.start()
+
+exec(open("start.py").read())
